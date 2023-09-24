fbe25d64bfe58b3151d3b42bc31d0b4c8d7c14c2
JusKoz88 <j.k.koziara@gmail.com>
2023-09-24 12:49:04 +0200

    Selenium 1

--- Selenium1.py
+++ Selenium1.py
@@ -1,3 +1,29 @@
 from selenium import webdriver
 import time
 import datetime
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+def make_screenshot(driver):
+    today = datetime.datetime.today()
+    short_date = today.strftime('_stamp%H%M%S')
+    screen_name = 'screen' + short_date + '.png'
+    driver.get_screenshot_as_file(screen_name)
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+driver.set_window_size(1600,800)
+username_field = driver.find_element('xpath','//*[@id="user-name"]')
+username_field.send_keys('standard_user')
+password_field = driver.find_element(By.XPATH,'//*[@id="password"]')
+password_field.send_keys('secret_sauce')
+
+login_button = driver.find_element(By.NAME,'login-button')
+login_button.click()
+time.sleep(3)
+driver.get_screenshot_as_file('screen.png')
+make_screenshot(driver)
+
+driver.quit
